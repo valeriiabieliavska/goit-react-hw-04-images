b39d3948d28a573ce154f707ea3d133a20ebda57
valeriiabieliavska <106553939+valeriiabieliavska@users.noreply.github.com>
2023-03-13 16:30:16 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,37 +2,34 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const Modal = ({ url, onClose }) => {
-const onEscape = event => {
-if (event.code === 'Escape') {
-onClose();
-}
-};
+  useEffect(() => {
+    const onEscape = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', onEscape);
+    return () => {
+      window.removeEventListener('keydown', onEscape);
+    };
+  }, [onClose]);
 
-useEffect(() => {
-window.addEventListener('keydown', onEscape);
-return () => {
-window.removeEventListener('keydown', onEscape);
-};
-}, []);
+  const handleModalClose = event => {
+    if (event.currentTarget === event.target) {
+      onClose();
+    }
+  };
 
-const handleModalClose = event => {
-if (event.currentTarget === event.target) {
-onClose();
-}
-};
-  
   return (
-<div className="Overlay" onClick={handleModalClose}>
-<div className="Modal">
-<img src={url} alt="IMG" />
-</div>
-</div>
-);
-}
-
+    <div className="Overlay" onClick={handleModalClose}>
+      <div className="Modal">
+        <img src={url} alt="IMG" />
+      </div>
+    </div>
+  );
+};
 
 Modal.propTypes = {
   url: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
 };
-
